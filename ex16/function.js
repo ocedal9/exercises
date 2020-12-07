5208f8abc93630b243370144f2ae72c5c4b7c8d6
ocedal9 <oguzman@tekmexico.com>
2020-12-07 12:41:25 -0800

    16

--- ex16/function.js
+++ ex16/function.js
@@ -1,15 +1,16 @@
 function hasLoop(ll) {
   let slow = ll.head.next
   let fast = ll.head.next.next
-  while (slow !== fast && fast && fast.next) {
+  while (fast.next && slow !== fast) {
     slow = slow.next
     fast = fast.next.next
   }
+  if (!fast.next) return false
   slow = ll.head
-  while (slow != fast && fast && fast.next) {
+  while (slow != fast) {
     slow = slow.next
     fast = fast.next
   }
-  return slow === fast ? slow : false
+  return slow
 }
 module.exports = hasLoop

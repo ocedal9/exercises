aa969c45285d6f0d3d36bf2a7a2813677d8ab04b
ocedal9 <oguzman@tekmexico.com>
2020-12-09 11:03:40 -0800

    17

--- ex17/function.js
+++ ex17/function.js
@@ -2,13 +2,13 @@ function isPal(ll) {
   let stack = []
   let currNode = ll.head
   while (currNode) {
-    stack.push(currNode)
+    stack.push(currNode.data)
     currNode = currNode.next
   }
   let pointer = ll.head
   while (pointer) {
     let elem = stack.pop()
-    if (elem.data !== pointer.data) return false
+    if (elem !== pointer.data) return false
     pointer = pointer.next
   }
   return true

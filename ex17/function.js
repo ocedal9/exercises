8a5128e1d89422632ccf3f39ce4936984950b987
ocedal9 <oguzman@tekmexico.com>
2020-12-09 15:37:40 -0800

    17

--- ex17/function.js
+++ ex17/function.js
@@ -1,22 +1,20 @@
 function isPal(ll) {
-  let middle = ll.head
-  let end = ll.head
-  while (end) {
-    middle = middle.next
-    end = end?.next?.next
+  let half = ll.head
+  let full = ll.head
+  while (full) {
+    half = half.next
+    full = full?.next?.next
   }
-  let secondHalf = null
-  while (middle) {
-    let temp = middle.next
-    middle.next = secondHalf
-    secondHalf = middle
-    middle = temp
+  let stack = []
+  while (half) {
+    stack.push(half.data)
+    half = half.next
   }
-  let firstHalf = ll.head
-  while (secondHalf) {
-    if (firstHalf.data !== secondHalf.data) return false
-    firstHalf = firstHalf.next
-    secondHalf = secondHalf?.next
+  let currNode = ll.head
+  while (stack.length > 0) {
+    const elem = stack.pop()
+    if (currNode.data !== elem) return false
+    currNode = currNode.next
   }
   return ll.head ? true : false
 }
